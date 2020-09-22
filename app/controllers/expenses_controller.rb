20e2d2242db99e324d4dc6ebd42a5244e6563b6e
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2020-09-22 17:35:16 +0200

    first part of new expense controller made

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,4 +5,20 @@ class ExpensesController < ApplicationController
     @flat = Flat.find(params[:flat_id])
     authorize @expense
   end
+
+  def create
+    @expense = Expense.new(expense_params)
+    authorize @expense
+    @expense.creator = current_user
+    @flat = Flat.find(params[:flat_id])
+    @expense.flat = @flat
+    @expense.save
+    raise
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:category, :amount, :description, :payment_month)
+  end
 end
