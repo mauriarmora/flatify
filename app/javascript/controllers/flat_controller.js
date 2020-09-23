0cc0b9559161eef54118e07e1fc2638ac12d4964
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-09-23 18:20:39 +0200

    started the cards for selecting flatmates and got the url for cloudinary

--- app/javascript/controllers/flat_controller.js
+++ app/javascript/controllers/flat_controller.js
@@ -9,12 +9,19 @@ export default class extends Controller {
     .then(response => response.json())
     .then((data) => {
       console.log(data)
-      const htmlEl = `<div>
-        <h5>${data.first_name}</h5>
+      const htmlEl = `<div class="new-mate-card">
+        <div class="add-mate-card">
+          <h5>${data.first_name}</h5>
+        </div>
+        <p>€${rentInput.value}</p>
+
          <input type="hidden" name="flatmate_ids[]" value="${data.id}" />
          <input type="hidden" name="rent[]" value="${rentInput.value}" />
       </div>`
       userContainer.insertAdjacentHTML("beforeend", htmlEl)
+      emailInput.innerHTML = "";
+      rentInput.innerHTML = "";
+
       // ad the card to the front end
     })
   }
