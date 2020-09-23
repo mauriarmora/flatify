b0f91bdd90ee826cfa9f6f19b6d229ea19a04e58
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2020-09-23 17:24:27 +0200

    new expense check box colors updated

--- app/javascript/functions/init_checkboxes.js
+++ app/javascript/functions/init_checkboxes.js
@@ -2,10 +2,13 @@ const initCheckboxes = () => {
   const checkboxes = document.querySelectorAll(".category-choice")
   checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("click", (e) => {
-      checkboxes.forEach((checkbox2) => {
-        checkbox2.classList.remove("active")
+      checkboxes.forEach((all_checkboxes) => {
+        all_checkboxes.style.background = "white"
+        all_checkboxes.classList.remove("active")
       })
+
       checkbox.classList.add("active")
+      checkbox.style.background = checkbox.dataset.color
     })
   })
 }
