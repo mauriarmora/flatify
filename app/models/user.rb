9bf4a324176b1eaa3c818ea084f194258bd30132
Guillem Delas <guillemdelas@hotmail.com>
2020-09-23 12:08:00 +0200

    added some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :rent, numericality: { only_integer: true }
 end
