bc7b191f01a1eb9ad4351551b26c26b040937d4a
Guillem Delas <guillemdelas@hotmail.com>
2020-09-23 12:14:52 +0200

    changed the user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, :email, :password, presence: true
 end
