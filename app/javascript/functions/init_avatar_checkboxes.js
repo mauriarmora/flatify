a75ba26ee6cf031f99fb4698dd8cc648b9a4c56c
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2020-09-25 12:44:24 +0200

    New Expense form styled to add photo and display photo, back button goes home, avatar selectabliity only for new expense page and name of flatmate positioned below pic

--- app/javascript/functions/init_avatar_checkboxes.js
+++ app/javascript/functions/init_avatar_checkboxes.js
@@ -1,5 +1,5 @@
 const initAvatarCheckboxes = () => {
-  const AvatarCheckboxes = document.querySelectorAll(".medium-avatar")
+  const AvatarCheckboxes = document.querySelectorAll(".expense-avatar")
   AvatarCheckboxes.forEach((avatar) => {
     avatar.addEventListener("click", (e) => {
       avatar.classList.toggle("active")
