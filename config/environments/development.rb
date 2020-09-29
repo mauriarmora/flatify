c7b6c8a15fc099409243adc6a266ab0a6c8e454f
Guillem Delas <guillemdelas@hotmail.com>
2020-09-29 10:24:24 +0200

    started working on the devise invitable gem and changed the design of the cards of the dashboard

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+config.action_mailer.default_url_options = { :host => "dev.yourhost.com" }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
