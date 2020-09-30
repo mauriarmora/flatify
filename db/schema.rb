c49fd809598feb06b14949ea5a0c98ef72f89f96
Maurici Armora <maurici.armora@gmail.com>
2020-09-30 16:54:52 +0200

    Added iban to flat schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_155200) do
+ActiveRecord::Schema.define(version: 2020_09_30_144514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_155200) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.float "rent"
+    t.string "iban"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
