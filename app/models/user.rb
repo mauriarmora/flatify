ecdee73ed994e1fda4692f0b34282547a5677ae8
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-10-01 16:36:27 +0200

    recalc admin rent

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  after_create :compute_admin_rent
+
   DEFAULT_AVATAR = 'https://res.cloudinary.com/dinkluxtp/image/upload/v1601458419/default-avatar-profile-trendy-style-social-media-user-icon-187599373_hkike6.jpg'
 
   def photo_url
@@ -32,5 +35,16 @@ class User < ApplicationRecord
     monthly_expenses = filter_by_expense_month(month)
 
     monthly_expenses.reduce(0) { |acc, expense| acc += expense.individual_expense }
+  end
+
+  def compute_admin_rent
+    admin = self.flat.admin
+    total_rent = self.flat.rent
+
+    flatmates = self.flat.users - [admin]
+
+    spare_rent = total_rent - flatmates.sum(:rent)
+
+    admin.update rent: spare_rent
   end
 end
