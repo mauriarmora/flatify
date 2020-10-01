b87e5f8a3c72a17c33c0e2b72307e7ccffd5aa9a
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-10-01 11:51:03 +0200

    changed the photo modal and implemented ajax for appending the comments

--- app/javascript/functions/init_img_modal.js
+++ app/javascript/functions/init_img_modal.js
@@ -10,10 +10,8 @@ const initModalImg = () => {
       captionText.innerHTML = this.alt;
     }
 
-    // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
       modal.style.display = "none";
     }
